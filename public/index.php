0431f4c74a8be39eb64f9e24bf6f00d60a94dfdc
Tom Vaillant <tom.vaillant.vt@gmail.com>
2025-06-19 11:25:59 +0200

    debuggage

--- public/index.php
+++ public/index.php
@@ -36,6 +36,7 @@ $router->add('account', ['controller' => 'User', 'action' => 'account', 'private
 $router->add('product', ['controller' => 'Product', 'action' => 'index', 'private' => true]);
 $router->add('contact', ['controller' => 'Product', 'action' => 'contact']);
 $router->add('product/{id:\d+}', ['controller' => 'Product', 'action' => 'show']);
+$router->add('api/products', ['controller' => 'Api', 'action' => 'products']);
 $router->add('{controller}/{action}');
 
 /*
